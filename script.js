a0fbdfdda9dacd96bd653310dc8d7eedf8bfb4be
James Ducker <james.ducker@ricewarner.com>
2015-09-01 16:28:48 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         domain.com.au junk listing filter
+// @namespace    http://github.com/jamesinc/
+// @version      0.1
+// @description  This extension filters 'application received' and 'deposit taken' style listings from summary view on the Domain website.
+// @author       James Ducker
+// @match        https://tampermonkey.net/index.php?version=3.11&ext=dhdg&updated=true
+// @grant        none
+// @include      http://www.domain.com.au/search/rent/*
+// @include      https://www.domain.com.au/search/rent/*
+// ==/UserScript==
+
+var isDickheadTitle=function ( title ) {
+	var titles = [ "application received", "deposit taken" ];
+	
+	for ( var i = 0; i < titles.length; i++ )
+		if (title.toLowerCase().indexOf(titles[i]) !== -1 ) return true;
+
+};
+
+$(".description__title, .description__main, .pricepoint").each(function( ) {
+	var el = $(this),
+	    parent;
+	    
+	if ( isDickheadTitle(el.text()) ) {
+		parent = el.parents("li.strap");
+		
+		parent.empty().css({
+			"line-height": 50,
+			background: "#999",
+			color: "white",
+			"text-align": "center"
+		});
+		
+		parent.text("Dick listing removed");
+	}
+});
